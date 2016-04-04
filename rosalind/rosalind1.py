1c4ec74dc64b289cb6454e88c4e326b48e88f162
michaelbilow <bilow@cs.ucla.edu>
2016-04-04 15:16:16 -0700

    initial commit

---
+++ rosalind/rosalind1.py
@@ -0,0 +1,15 @@
+
+
+if __name__ == "__main__":
+    print 'Holla World'
+
+    a = 851
+    b = 894
+
+    output = a**2 + b**2
+
+    print output
+
+    fn = 'camelot.txt'
+    with open(fn, 'r') as f:
+        for anything in f:
